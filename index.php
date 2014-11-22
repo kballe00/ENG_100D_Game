5e411016b00c149e5098eb24a412c9d1716ab92e
Purag Moumdjian <purmou@gmail.com>
2014-11-21 19:46:19 -0800

    Adding jQuery

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!doctype html>
 <html>
 <head>
-
+  <title></title>
+  
+  <link href="style.css" rel="stylesheet">
+  
 </head>
 <body>
 
+  <script>
+    (function( d ){
+      var jQ = d.createElement("script"),
+          l  = d.head.getElementsByTagName("link")[0];
+      
+      jQ.src = "//code.jquery.com/jquery-latest.min.js";
+      
+      d.head.appendChild( jQ, l );
+    })( document );
+  </script>
 </body>
 </html>
